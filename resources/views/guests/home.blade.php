1fdfb7af1f7ab524b68f4f0678c45aead16eccfc
Obez99 <giuliogx@live.it>
2022-01-17 17:20:03 +0100

    Imports Vue into project

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app') --}}
 
 @section('content')
     <div id="root"></div>
 
-    <script src="{{asset('js/vue.js')}}"></script>
+    <script src="{{asset('js/front.js')}}"></script>
 @endsection
