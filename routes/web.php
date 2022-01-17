d8379d6fbc0478a74672e8d72b28c8c18e4ebdfc
Obez99 <giuliogx@live.it>
2022-01-17 15:52:12 +0100

    Creates basic login and register with laravel ui

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware("auth")
+    ->namespace("Admin")
+    ->prefix("admin")
+    ->name("admin.")
+    ->group(function () {
+        Route::get("/", "HomeController@index")->name("home");
+    });
+
+Route::get("{any?}", function () {
+    return view("guests.home");
+})->where("any", ".*");
