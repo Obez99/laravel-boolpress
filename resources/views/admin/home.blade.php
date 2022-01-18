4595fe9e668af453605107b19dbcdda1344306ba
Obez99 <giuliogx@live.it>
2022-01-18 19:59:46 +0100

    Completes "my posts" section

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.admin')
 
 @section('content')
-<ul class="list-group">
+<ul class="list-group user-posts">
   @foreach ($posts as $post)
-    <li class="list-group-item d-flex justify-content-between">
-      <a href="">{{$post->title}}</a>
-      <strong>{{$post->created_at}}</strong>
+    <li class="list-group-item p-0">
+      <a href="{{route('admin.posts.show')}}" class="d-flex justify-content-between p-3">{{$post->title}}
+        <strong>{{$post->created_at}}</strong>
+      </a>
     </li>
   @endforeach
   
